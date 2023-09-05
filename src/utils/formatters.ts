8428ff5f146c906bf14b36f2046559a44ac0a116
Odailson Silva <dev.odailson2021@gmail.com>
2023-09-05 20:44:19 -0300

    feat: masks

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,91 +1,131 @@
 export const removeMask = (cpf_cnpj: string) => {
-  if (!cpf_cnpj) return
-  return cpf_cnpj.replace(/[^\d]+/g, '')
-}
+  if (!cpf_cnpj) return;
+  return cpf_cnpj.replace(/[^\d]+/g, "");
+};
 
 export const formatToIdObject = (object: any, fieldName: string) => {
   if (!object[fieldName] || !object) {
-    return
+    return;
   }
 
-  if (typeof object[fieldName] === 'number') return { id: object[fieldName] }
+  if (typeof object[fieldName] === "number") return { id: object[fieldName] };
 
-  return object[fieldName]
-}
+  return object[fieldName];
+};
 export const formatFromIdObject = (
   object: any,
   fieldName: string
 ): undefined | number => {
   if (!object[fieldName] || !object[fieldName]?.id) {
-    return
+    return;
   }
 
-  return object[fieldName].id
-}
+  return object[fieldName].id;
+};
 
 export function formatBytes(bytes: number, decimals = 2) {
-  if (bytes === 0) return '0 Bytes'
+  if (bytes === 0) return "0 Bytes";
 
-  const kb = 1024
-  const dm = decimals < 0 ? 0 : decimals
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const kb = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
-  const i = Math.floor(Math.log(bytes) / Math.log(kb))
+  const i = Math.floor(Math.log(bytes) / Math.log(kb));
 
-  return parseFloat((bytes / Math.pow(kb, i)).toFixed(dm)) + ' ' + sizes[i]
+  return parseFloat((bytes / Math.pow(kb, i)).toFixed(dm)) + " " + sizes[i];
 }
 
-export const { format: formatNumberToApi } = new Intl.NumberFormat('en-US', {
-  minimumFractionDigits: 2
-})
-export const { format: formatNumberToDouble } = new Intl.NumberFormat('en-US', {
-  minimumFractionDigits: 2
-})
-
-export const percentageFormatter = new Intl.NumberFormat('pt-BR', {
-  style: 'percent',
+export const { format: formatNumberToApi } = new Intl.NumberFormat("en-US", {
   minimumFractionDigits: 2,
-  maximumFractionDigits: 2
-})
-export const intFormatter = new Intl.NumberFormat('pt-BR', {
-  minimumFractionDigits: 0,
-  maximumFractionDigits: 0
-})
+});
 
-export const currencyFormatter = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL'
-})
-export const currencyIntFormatter = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
+export const percentageFormatter = new Intl.NumberFormat("pt-BR", {
+  style: "percent",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+export const intFormatter = new Intl.NumberFormat("pt-BR", {
   minimumFractionDigits: 0,
-  maximumFractionDigits: 0
-})
-export const intCurrencyFormatter = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
+  maximumFractionDigits: 0,
+});
+
+export const currencyFormatter = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+export const currencyIntFormatter = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
   minimumFractionDigits: 0,
-  maximumFractionDigits: 0
-})
+  maximumFractionDigits: 0,
+});
+export const intCurrencyFormatter = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+});
 export const formatNumberWithComma = (value: any) =>
-  new Intl.NumberFormat('pt-BR', { maximumFractionDigits: 2 })
+  new Intl.NumberFormat("pt-BR", { maximumFractionDigits: 2 })
     .format(value)
-    .replace('.', ',')
+    .replace(".", ",");
 
 export const dataSheetFormatter = (
   value: any,
   isOneDecimalPlace?: boolean
 ): string => {
-  const formatedNumber = Intl.NumberFormat('pt-BR', {
-    maximumFractionDigits: 2
+  const formatedNumber = Intl.NumberFormat("pt-BR", {
+    maximumFractionDigits: 2,
   })
-    .format(value || '')
-    .replace(',', '.')
+    .format(value || "")
+    .replace(",", ".");
 
   if (isOneDecimalPlace) {
-    return Number(formatedNumber).toFixed(1)
+    return Number(formatedNumber).toFixed(1);
   }
 
-  return formatedNumber
+  return formatedNumber;
+};
+
+export interface ITelefone {
+  ddd: { ddd: string; descricao: string };
+  ddi: { ddi: string; descricao: string };
+  telefone: string;
 }
+export const getPhoneInfo = (telefone: ITelefone | undefined) => {
+  const ddi = telefone?.ddi?.ddi ? +telefone?.ddi?.ddi : "";
+  const ddd = telefone?.ddd?.ddd ? +telefone?.ddd?.ddd : "";
+  const phone = telefone?.telefone || "";
+
+  return { ddi, ddd, phone };
+};
+export const getFullPhone = (telefone: ITelefone | undefined) => {
+  const { ddi, ddd, phone } = getPhoneInfo(telefone);
+
+  return `${ddi}${ddd}${phone}`;
+};
+export const getDDDPhone = (telefone: ITelefone | undefined) => {
+  const { ddd, phone } = getPhoneInfo(telefone);
+
+  return `${ddd}${phone}`;
+};
+
+export const formatAccountNumber = (
+  accountNumber?: string,
+  dv?: string
+): string => {
+  const normalizedAccount = removeMask(accountNumber || "");
+  if (!normalizedAccount) return "";
+
+  if (dv) {
+    return `${normalizedAccount} - ${dv}`;
+  }
+
+  const len = normalizedAccount.length;
+  const formattedAccountNumber =
+    normalizedAccount.substring(0, len - 1) +
+    "-" +
+    normalizedAccount.substring(len - 1);
+
+  return formattedAccountNumber;
+};
--- src/utils/transforms.ts
+++ src/utils/transforms.ts
@@ -1,352 +1,383 @@
-/* eslint-disable @typescript-eslint/ban-types */
 import {
   dateIsoToLocal,
   dateLocalToIso,
   dateUTCIsoToLocal,
-  dateUTCLocalToIso
-} from './date'
+  dateUTCLocalToIso,
+} from "./date";
 import {
   currencyFormatter,
   currencyIntFormatter,
-  dataSheetFormatter,
-  formatNumberToApi,
-  removeMask
-} from './formatters'
+  removeMask,
+} from "./formatters";
 
-import masker from 'vanilla-masker'
+import masker from "vanilla-masker";
 
 export const moneyInputTransform = {
   input: (value: any, roundNumber?: boolean) => {
-    if (isNaN(value) || value === '') return ''
-    if (value == '0') {
-      return '0'
+    if (!value) return "";
+
+    if (isNaN(value) || value === "") return "";
+    if (value == "0") {
+      return "0";
     }
 
     const _value = roundNumber
-      ? currencyFormatter.format(Math.round(value || 0)).replace('R$ ', '')
-      : currencyFormatter.format(value).replace('R$ ', '')
+      ? currencyFormatter.format(Math.round(value || 0)).replace("R$ ", "")
+      : currencyFormatter.format(value).replace("R$ ", "");
 
-    return _value
+    return _value;
   },
   output: (e: any, onChange: Function, roundNumber?: boolean) => {
-    let value = e.target.value
+    let value = e.target.value;
 
-    if (value === '') {
-      onChange(value)
-      return
+    if (value === "") {
+      onChange(value);
+      return;
     }
-    if (value === '0') {
-      onChange(value)
-      return
+    if (value === "0") {
+      onChange(+value);
+      return;
     }
 
-    value = removeMask(value)
+    const isNegative = value.includes("-");
+
+    value = removeMask(value);
 
     if (value.length === 1) {
-      value = '0.0' + value
+      value = "0.0" + value;
     } else {
       value =
         value.slice(0, value.length - 2) +
-        '.' +
-        value.slice(value.length - 2, value.length)
+        "." +
+        value.slice(value.length - 2, value.length);
     }
 
     const output = roundNumber
       ? parseFloat(Math.round(parseFloat(value || 0)).toFixed(2))
-      : parseFloat(parseFloat(value).toFixed(2))
+      : parseFloat(parseFloat(value).toFixed(2));
 
-    const v = isNaN(output) ? 0 : output
+    const v = isNaN(output) ? 0 : output;
 
-    onChange(v)
-  }
-}
+    onChange(isNegative ? -v : v);
+  },
+};
+
+export const alphaNumericInputTransform = {
+  input: (value: string) => {
+    return value || "";
+  },
+  output: (e: any, onChange: Function) => {
+    const rawValue = e.target.value || "";
+
+    const value = rawValue.replace(/\W/g, "");
+
+    onChange(value);
+  },
+};
 export const moneyIntInputTransform = {
   input: (value: any) => {
     //* deve sempre retornar string
 
-    if (isNaN(value)) return ''
-    if (value == '0') {
-      return '0'
+    if (isNaN(value)) return "";
+    if (value == "0") {
+      return "0";
     }
 
     const _value = currencyIntFormatter
       .format(value)
-      .replace('R$ ', '')
-      .split(',')[0]
+      .replace("R$ ", "")
+      .split(",")[0];
 
-    return _value
+    return _value;
   },
   output: (e: any, onChange: (value: number) => void) => {
     //* formatar para o numero inteiro
 
-    const checkSignal = e.target.value.length > 0 && e.target.value[0]
+    const checkSignal = e.target.value.length > 0 && e.target.value[0];
 
-    const value =
-      checkSignal === '-'
+    let value =
+      checkSignal === "-"
         ? checkSignal + removeMask(e.target.value)
-        : removeMask(e.target.value)
+        : removeMask(e.target.value);
 
-    const output = parseInt(value, 10)
-    const v = isNaN(output) ? 0 : output
+    const output = parseInt(value, 10);
+    const v = isNaN(output) ? 0 : output;
 
-    onChange(v)
-  }
-}
+    onChange(v);
+  },
+};
 
 export const dateInputTransform = {
   input: (value: any) => {
-    return (value && dateIsoToLocal(value)) || ''
+    return (value && dateIsoToLocal(value)) || "";
   },
   output: (e: any, onChange: Function) => {
     //* formatar para o ISO
 
-    const value = dateLocalToIso(e.target.value)
+    const value = dateLocalToIso(e.target.value);
 
-    const v = value ? value : undefined
+    const v = value ? value : undefined;
 
-    onChange(v)
-  }
-}
+    onChange(v);
+  },
+};
 export const dateInputWithResetTimeTransform = {
   input: (value: any) => {
-    return (value && dateUTCIsoToLocal(value)) || ''
+    return (value && dateUTCIsoToLocal(value)) || "";
   },
   output: (e: any, onChange: Function) => {
     //* formatar para o ISO
 
-    const value = dateUTCLocalToIso(e.target.value, true)
+    const value = dateUTCLocalToIso(e.target.value, true);
 
-    const v = value ? value : undefined
+    const v = value ? value : undefined;
 
-    onChange(v)
-  }
-}
+    onChange(v);
+  },
+};
 export const monthNames = [
-  'jan',
-  'feb',
-  'mar',
-  'abr',
-  'mai',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'out',
-  'nov',
-  'dez'
-]
+  "jan",
+  "feb",
+  "mar",
+  "abr",
+  "mai",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "out",
+  "nov",
+  "dez",
+];
 export const monthInputTransform = {
   input: (value?: string) => {
-    if (!value) return ''
-    const [month, year] = value.split('/')
-    const monthIndex = monthNames.findIndex((m) => m == month)
-    return `${year}-${(monthIndex + 1).toString().padStart(2, '0')}`
+    if (!value) return "";
+    const [month, year] = value.split("/");
+    const monthIndex = monthNames.findIndex((m) => m == month);
+    return `${year}-${(monthIndex + 1).toString().padStart(2, "0")}`;
   },
   output: (e: any, onChange: Function) => {
-    const [year, month] = e.target.value.split('-')
-    const v = `${monthNames[month - 1]}/${year}`
+    const [year, month] = e.target.value.split("-");
+    const v = `${monthNames[month - 1]}/${year}`;
 
-    onChange(v)
-  }
-}
+    onChange(v);
+  },
+};
 
 export const cpfCnpjInputTransform = {
   input: (value?: string) => {
     //* deve sempre retornar string
 
     if (!value) {
-      return ''
+      return "";
     }
 
-    const masks = ['999.999.999-99']
+    const masks = ["999.999.999-99"];
 
-    return dynamicMaskHandler(masks, 11, value)
+    return dynamicMaskHandler({ masks, lengthCheckPoints: [11], value });
   },
   output: (e: any, onChange: Function) => {
-    const value = removeMask(e.target.value) || ''
+    const value = removeMask(e.target.value) || "";
 
-    if (value.length > 14) return
+    if (value.length > 14) return;
 
-    onChange(value)
-  }
-}
+    onChange(value);
+  },
+};
 export const phoneInputTransform = {
   input: (value?: string) => {
     //* deve sempre retornar string
 
     if (!value) {
-      return ''
+      return "";
     }
 
-    const masks = ['(99) 9999-9999', '(99) 99999-9999']
+    const masks = ["(99) 9999-9999", "(99) 99999-9999"];
 
-    return dynamicMaskHandler(masks, 10, value)
+    return dynamicMaskHandler({ masks, lengthCheckPoints: [10], value });
   },
   output: (e: any, onChange: Function) => {
-    const value = removeMask(e.target.value) || ''
+    const value = removeMask(e.target.value) || "";
 
-    if (value.length > 11) return
+    if (value.length > 12) return;
 
-    onChange(value)
-  }
-}
+    onChange(value);
+  },
+};
 export const cepInputTransform = {
   input: (value?: string) => {
     //* deve sempre retornar string
 
     if (!value) {
-      return ''
+      return "";
     }
 
-    return masker.toPattern(value, '99.999-999')
+    return masker.toPattern(value, "99.999-999");
   },
   output: (e: any, onChange: Function) => {
-    const value = removeMask(e.target.value) || ''
+    const value = removeMask(e.target.value) || "";
 
-    if (value.length > 8) return
+    if (value.length > 8) return;
 
-    onChange(value)
-  }
-}
+    onChange(value);
+  },
+};
 export const rgInputTransform = {
   input: (value?: string) => {
     //* deve sempre retornar string
 
     if (!value) {
-      return ''
+      return "";
     }
 
-    return masker.toPattern(value, '99.999.999-S')
+    return masker.toPattern(value, "99.999.999-S");
   },
   output: (e: any, onChange: Function) => {
-    const value = removeMask(e.target.value) || ''
+    const value = removeMask(e.target.value) || "";
 
-    if (value.length > 9) return
+    if (value.length > 9) return;
 
-    onChange(value)
-  }
-}
+    onChange(value);
+  },
+};
 export const intInputTransform = {
   input: (value: any) => {
     //* deve sempre retornar string
 
     if (value === 0) {
-      return '0'
+      return "0";
     }
 
     if (isNaN(+value) || !value) {
-      return ''
+      return "";
     }
-    return parseInt(value).toString()
+    return parseInt(value).toString();
   },
   output: (e: any, onChange: Function) => {
     //* formatar para o numero inteiro
 
-    const output = parseInt(e.target.value, 10)
-    const v = isNaN(output) ? undefined : output
+    const output = parseInt(e.target.value, 10);
+    const v = isNaN(output) ? undefined : output;
 
-    onChange(v)
-
-    return v
-  }
-}
-export const doubleInputTransform = {
-  input: (value: any, roundNumber?: boolean) => {
-    if (isNaN(value) || value === '') return ''
-    if (value == '0') {
-      return '0'
-    }
-
-    const _value = formatNumberToApi(value).replaceAll(',', '.')
-    return _value
+    onChange(v);
   },
-  output: (e: any, onChange: Function, roundNumber?: boolean) => {
-    let value = e.target.value
+};
 
-    if (value === '') {
-      onChange(value)
-      return
-    }
-    if (value === '0') {
-      onChange(value)
-      return
-    }
+export const doubleNInputTransform = (decimals: number) => ({
+  input: (value: any) => {
+    //* deve sempre retornar string
+    if (isNaN(value) || value === "" || !value) return "";
 
-    value = removeMask(value)
+    const [wholePart = "00", integralPart = ""] = `${value.toFixed(
+      decimals
+    )}`.split(".");
 
-    if (value.length === 1) {
-      value = '0.0' + value
-    } else {
-      value =
-        value.slice(0, value.length - 2) +
-        '.' +
-        value.slice(value.length - 2, value.length)
-    }
+    return `${wholePart},${integralPart.padStart(decimals, "0")}`;
+  },
+  output: (e: any, onChange: Function) => {
+    //* formatar para o numero com duas casas decimais
+    const value = removeMask(e.target.value);
 
-    const output = roundNumber
-      ? parseFloat(Math.round(parseFloat(value || 0)).toFixed(2))
-      : parseFloat(value)
+    if (!value) return onChange(0);
 
-    const v = isNaN(output) ? 0 : output
+    const integralPart = value.slice(-decimals);
+    const wholePart = value.slice(0, value.length - decimals);
 
-    onChange(v)
+    console.log({ value, integralPart, wholePart });
+    const formattedValue = +`${wholePart}.${integralPart.padStart(
+      decimals,
+      "0"
+    )}`;
 
-    return v
-  }
-}
+    const output = formattedValue;
+
+    const v = isNaN(output) ? "" : output;
+
+    onChange(v);
+  },
+});
+export const doubleInputTransform = { ...doubleNInputTransform(2) };
 
 export const fileInputTransform = {
   output: (e: any, onChange: Function) => {
-    const file: File = e.target.files[0]
+    const file: File = e.target.files[0];
 
-    onChange({ file, originalFilename: file.name })
+    onChange({ file, originalFilename: file.name });
   },
   input: (value: {
-    file?: File
-    originalFilename: string
-    filename?: string
+    file?: File;
+    originalFilename: string;
+    filename?: string;
   }) => {
     if (!value?.file) {
-      return ''
+      return "";
     }
 
     const newValue = value?.originalFilename
       ? `C:\\fakepath\\${value.originalFilename}`
-      : ''
-    return newValue
-  }
-}
+      : "";
+    return newValue;
+  },
+};
 
 type TSelectTransform<T = any> = {
-  input: (value: T) => string | number | Array<string | number | object>
+  input: (value: T) => string | number | Array<string | number | object>;
   output: (
     e: { target: { value?: string | number | object | undefined | unknown } },
     onChange: (value?: T) => void
-  ) => void
-}
+  ) => void;
+};
 
 export const selectObjectIdTransform: TSelectTransform<{ id: number }> = {
   input: (v) => {
-    return v?.id ?? ''
+    return v?.id ?? "";
   },
   output: (e, onChange) => {
     onChange(
-      e.target?.value && typeof e.target.value === 'number'
+      e.target?.value && typeof e.target.value === "number"
         ? { id: e.target.value }
         : undefined
-    )
-  }
-}
+    );
+  },
+};
+
+export const stringLengthInputTransform = (stringLength: number) => ({
+  input: (value?: string) => {
+    return value || "";
+  },
+  output: (e: any, onChange: Function) => {
+    const value: string = e.target.value || "";
+
+    if (value.length >= stringLength) {
+      return onChange(value.slice(0, stringLength));
+    }
+
+    return onChange(value);
+  },
+});
 
 //** https://vanilla-masker.github.io/vanilla-masker/ */
 
-export const dynamicMaskHandler = (
-  masks: string[],
-  max: number,
-  value: string
-): string => {
-  const c = removeMask(value) as string
-  const m = c.length > max ? 1 : 0
-
-  return masker.toPattern(c, masks[m])
+interface IDynamicMaskHandler {
+  masks: string[];
+  lengthCheckPoints: number[];
+  value: string;
 }
+
+export const dynamicMaskHandler = ({
+  masks,
+  lengthCheckPoints,
+  value,
+}: IDynamicMaskHandler): string => {
+  const noMaskValue = removeMask(value) as string;
+
+  const maskIndex = lengthCheckPoints.reduce((acc, curr, index) => {
+    if (noMaskValue.length > curr) {
+      return index + 1;
+    }
+
+    return acc;
+  }, 0);
+
+  return masker.toPattern(noMaskValue, masks[maskIndex]);
+};
